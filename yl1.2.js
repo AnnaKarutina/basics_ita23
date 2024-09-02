193383b5c646b80edfafffe78a78b25ae7327d58
Anna Karutina <anna.karutina@khk.ee>
2024-09-02 15:19:06 +0300

    Ylesanne 1.2 - string template

--- yl1.2.js
+++ yl1.2.js
@@ -1,5 +1,5 @@
 let aasta = 2016
 let president = 'Kersti Kaljulaid'
 let lauseKeskosa = '. aastal valiti Eesti presidendiks '
-let lause = aasta + lauseKeskosa + president
+let lause = `${aasta}. aastal valiti Eesti presidendiks ${president}`
 console.log(lause)
